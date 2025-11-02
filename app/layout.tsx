b23957f711c76a538c795649a4bc03773fedd0b5
opsprotocoltools <opsprotocoltools@gmail.com>
2025-11-01 21:01:41 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import '../global.d.ts';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ops Protocol Tools",
-  description: "Automate clarity. Monetize discipline.",
+  description: "Automate clarity, Monetize Discipline",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
