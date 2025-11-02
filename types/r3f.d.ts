6bc363e4788154d8c1c5c064885aeb00ccdd469e
opsprotocoltools <opsprotocoltools@gmail.com>
2025-11-01 21:14:52 -0400

    Update r3f.d.ts

--- types/r3f.d.ts
+++ types/r3f.d.ts
@@ -1,1 +1,14 @@
-/// <reference types="@react-three/fiber" />
+// /types/r3f.d.ts
+import type { ThreeElements } from "@react-three/fiber";
+
+declare global {
+  namespace JSX {
+    // Add all @react-three/fiber JSX tags (points, mesh, etc.)
+    interface IntrinsicElements extends ThreeElements {
+      // Allow `<primitive />` when you pass raw THREE objects
+      primitive: any;
+    }
+  }
+}
+
+export {};
